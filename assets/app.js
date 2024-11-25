8a2820d25f31f1d2ffdbca638c3a9514c687ebb4
johlbaum <julien.ohlbaum@gmail.com>
2024-11-25 22:17:17 +0100

    Added fade-out effect to error and confirmation messages.

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,13 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import './styles/app.min.css';
+
+document.addEventListener('turbo:load', function () {
+  const flashMessages = document.querySelectorAll('.flash-message');
+  flashMessages.forEach((message) => {
+    setTimeout(() => {
+      message.style.opacity = '0';
+      setTimeout(() => message.remove(), 1000);
+    }, 3000);
+  });
+});
